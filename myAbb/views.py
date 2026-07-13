d57bfdab64790d5e7edad43eb793c73027954f02
Kanyaphon Chaichana <kanyaporn4890@gmail.com>
2026-07-13 10:43:39 +0700

    A

--- myAbb/views.py
+++ myAbb/views.py
@@ -1,8 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello")
-def contact(request):
-    return HttpResponse("Contact")
-def about(request):
-    return HttpResponse("About")
+    return render (request,'index.html')
